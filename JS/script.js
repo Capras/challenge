c742a4dccce9be6542d3c3e7c350bf4c142f3498
Sara Capra <scapra@techtonicgroup.com>
2018-05-11 18:09:16 -0400

    update

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,13 @@ function QuestionMarks(str) {
      if (!matchedString) return false;
     for (var i = 0; i < this.matchedString.length; i++)
     for(let j = i+1; j < this.matchedString.length; j++) {
-          if(Number(matchedString[i])+ Number(matchedString[j]) === 10) {
-            if(matchedString.slice(i+1,j).split('?').length - 1 === 3) {
-                return true;
-            }
-        } else {
-          return false
+      if(Number(matchedString[i])+ Number(matchedString[j]) === 10) {
+        if(matchedString.slice(i+1,j).split('?').length - 1 === 3) {
+            return true;
+        }
+      } else {
+        return false
+        }
 }
 
 console.log(QuestionsMarks("arrb6???4xxbl5???eee5"))
